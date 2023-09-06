062fd5f3c33ebc4ed37d8a8e80f2594158dfa971
DavidFranco3 <josedavidayalafranco3@gmail.com>
2023-09-06 17:57:45 -0600

    correcciones en patrocinadores

--- src/routes/patrocinadores.routes.js
+++ src/routes/patrocinadores.routes.js
@@ -34,6 +34,16 @@ router.get("/listar", async (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
+// Obtener todas los patrocionadores
+router.get("/listarPorPrioridad", async (req, res) => {
+    const { prioridadAparicion } = req.query;
+    patrocinadores
+        .find({ prioridadAparicion })
+        .sort({ _id: -1 })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
 // Obtener las ventas activas con paginacion
 router.get("/listarPaginandoActivos", async (req, res) => {
     const { pagina, limite } = req.query;
